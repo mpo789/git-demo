4640612bc39de50208a876d53ca0c7d9f553bda2
阿威 <82465206@qq.com>
2021-09-13 00:17:56 +0800

    主分支最大数3提交

---
+++ src/main/java/com/wei/ditTest/GitTest.java
@@ -0,0 +1,9 @@
+package com.wei.ditTest;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+    }
+}
